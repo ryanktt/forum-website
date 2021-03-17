af3688a8622db198b3ab2da55aa8fa82c9eba02f
ryanktt <ryankayrorv@outlook.com>
2021-03-17 16:58:47 -0300

    add dinamic port

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get('/', (req,res) => {
     res.send('<h1>test</h1>')
 })
  
-
-app.listen(5000, () => {
-    console.log('running on port 5000')
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log('running on port' + port);
 })
